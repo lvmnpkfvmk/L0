32efe241112233db9c6b1d4df78118eca32a1a35
lvmnpkfvmk <tinbeetray@gmail.com>
2023-08-28 16:10:19 +0300

    :wrench: Add Config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	// "context"
+	"fmt"
+	"log"
 
+	"github.com/lvmnpkfvmk/L0/config"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// ctx := context.Background()
+	cfg := config.Get()
+	fmt.Println(cfg)
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lvmnpkfvmk/L0
 
 go 1.21.0
+
+require github.com/kelseyhightower/envconfig v1.4.0 // indirect
